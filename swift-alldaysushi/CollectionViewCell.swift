aef1d93f7101456ec7bd0e715a46c4745d4733ab
GewoonRene <rhuibertsjr@gmail.com>
2019-10-11 11:20:57 +0200

    Added: GridLayout

---
+++ swift-alldaysushi/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  swift-alldaysushi
+//
+//  Created by René Jr Huiberts on 11/10/2019.
+//  Copyright © 2019 René Jr Huiberts. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
--- swift-alldaysushi/FirstViewController.swift
+++ swift-alldaysushi/FirstViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,
+UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let items = ["0","1","2","3","4","5","6","7","8","9","10",];
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count;
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: <#T##IndexPath#>) as! CollectionViewCell
+        
+        cell.cellLabel.text = items[indexPath.item];
+        
+        return cell;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item);
+    }
 
 }
 
